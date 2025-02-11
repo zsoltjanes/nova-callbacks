c0ab5bdff413226f2dfe69649d587302f5ebf7a5
Zsolt Janes <zsolt.janes@gmail.com>
2025-02-11 10:41:56 +0100

    Add pivot actions

--- src/HasCallbacks.php
+++ src/HasCallbacks.php
@@ -15,59 +15,96 @@ use Laravel\Nova\Http\Requests\NovaRequest;
  */
 trait HasCallbacks
 {
-    public static function fill(NovaRequest $request, $model)
+    public static function fill(NovaRequest $request, $model): array
     {
-        if (method_exists(static::class, 'beforeSave')) {
-            static::beforeSave($request, $model);
-        }
+        static::executeCallbacks('beforeSave', $request, $model);
+        static::executeCallbacks('beforeCreate', $request, $model);
+        static::registerSavedCallbacks('afterSave', $request, $model);
+        static::registerCreatedCallbacks('afterCreate', $request, $model);
 
-        if (method_exists(static::class, 'beforeCreate')) {
-            static::beforeCreate($request, $model);
-        }
-
-        if (method_exists(static::class, 'afterSave')) {
-            $model::saved(function ($model) use ($request) {
-                static::afterSave($request, $model);
-            });
-        }
-
-        if (method_exists(static::class, 'afterCreate')) {
-            $model::created(function ($model) use ($request) {
-                static::afterCreate($request, $model);
-            });
-        }
-        
         return static::fillFields(
             $request, $model,
-            (new static($model))->creationFieldsWithoutReadonly($request)
+            (new static())
+                ->creationFields($request)
+                ->applyDependsOn($request)
+                ->withoutReadonly($request)
+                ->withoutUnfillable()
         );
     }
 
-    public static function fillForUpdate(NovaRequest $request, $model)
+    public static function fillForUpdate(NovaRequest $request, $model): array
     {
-        if (method_exists(static::class, 'beforeSave')) {
-            static::beforeSave($request, $model);
-        }
-
-        if (method_exists(static::class, 'beforeUpdate')) {
-            static::beforeUpdate($request, $model);
-        }
-
-        if (method_exists(static::class, 'afterSave')) {
-            $model::saved(function ($model) use ($request) {
-                static::afterSave($request, $model);
-            });
-        }
-        
-        if (method_exists(static::class, 'afterUpdate')) {
-            $model::saved(function ($model) use ($request) {
-                static::afterUpdate($request, $model);
-            });
-        }
+        static::executeCallbacks('beforeSave', $request, $model);
+        static::executeCallbacks('beforeUpdate', $request, $model);
+        static::registerSavedCallbacks('afterSave', $request, $model);
+        static::registerSavedCallbacks('afterUpdate', $request, $model);
 
         return static::fillFields(
             $request, $model,
-            (new static($model))->updateFieldsWithoutReadonly($request)
+            (new static())
+                ->updateFields($request)
+                ->applyDependsOn($request)
+                ->withoutReadonly($request)
+                ->withoutUnfillable()
         );
+    }
+
+    public static function fillPivot(NovaRequest $request, $model, $pivot): array
+    {
+        static::executeCallbacks('beforeSave', $request, $pivot);
+        static::executeCallbacks('beforeCreate', $request, $pivot);
+        static::registerSavedCallbacks('afterSave', $request, $pivot);
+        static::registerCreatedCallbacks('afterCreate', $request, $pivot);
+
+        return static::fillFields(
+            $request, $pivot,
+            (new static())
+                ->creationPivotFields($request, $request->relatedResource)
+                ->applyDependsOn($request)
+                ->withoutReadonly($request)
+                ->withoutUnfillable()
+        );
+    }
+
+    public static function fillPivotForUpdate(NovaRequest $request, $model, $pivot): array
+    {
+        static::executeCallbacks('beforeSave', $request, $pivot);
+        static::executeCallbacks('beforeUpdate', $request, $pivot);
+        static::registerSavedCallbacks('afterSave', $request, $pivot);
+        static::registerSavedCallbacks('afterUpdate', $request, $pivot);
+
+        return static::fillFields(
+            $request, $pivot,
+            (new static())
+                ->updatePivotFields($request, $request->relatedResource)
+                ->applyDependsOn($request)
+                ->withoutReadonly($request)
+                ->withoutUnfillable()
+        );
+    }
+
+    protected static function executeCallbacks(string $method, NovaRequest $request, $model): void
+    {
+        if (method_exists(static::class, $method)) {
+            static::{$method}($request, $model);
+        }
+    }
+
+    protected static function registerSavedCallbacks(string $method, NovaRequest $request, $model): void
+    {
+        if (method_exists(static::class, $method)) {
+            $model::saved(function ($model) use ($request, $method) {
+                static::{$method}($request, $model);
+            });
+        }
+    }
+
+    protected static function registerCreatedCallbacks(string $method, NovaRequest $request, $model): void
+    {
+        if (method_exists(static::class, $method)) {
+            $model::created(function ($model) use ($request, $method) {
+                static::{$method}($request, $model);
+            });
+        }
     }
 }
